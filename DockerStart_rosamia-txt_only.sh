7b40e1d3c618b4ed1657796c44b2d4b290e91122
Your Name <you@example.com>
2017-10-19 13:18:50 +0900

    docker start shell modifiy, current dir setting form pwd command

--- DockerStart_rosamia-txt_only.sh
+++ DockerStart_rosamia-txt_only.sh
@@ -1,1 +1,3 @@
-docker run -i -p 5000:5000 -v /Users/devcraft3/dev/ws/rosamia/rasa_nlu/rosamia-txt:/app/rosamia-txt rasa_nlu start -c ./rosamia-txt/config_mitie_docker.json   --server_model_dirs=/app/rosamia-txt/models/model_20170929-043427
+pwd=$(pwd)
+#echo $pwd
+sudo docker run -d -i -p 5000:5000 -v $pwd/rosamia-txt:/app/rosamia-txt rasa_nlu start -c ./rosamia-txt/config_mitie_docker.json   --server_model_dirs=/app/rosamia-txt/models/model_20170929-043427
